6d506cd3d3fc28674b7b7274b7e1a06c88e0a292
igorseynaeve12 <igor.seynaeve@student.vives.be>
2024-05-21 19:11:01 +0200

    parking toegevoegd

--- app.js
+++ app.js
@@ -2,22 +2,31 @@
 //Express API
 const express = require('express');
 const app = express();
-const router = express.Router();
-const fs = require('fs');
+
+
+
+
+
 const config = require('config');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const parking = require('./routes/parkings');
+const fs = require('fs');
 const port = process.env.PORT || 4500;
 
 
 
+app.use('/api', parking);
+app.use(helmet());
+
 // Database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test')
+mongoose.connect('mongodb://localhost/opdrachtNode')
 .then(()=> console.log('Verbonden met MongoDB'))
 .catch(err => console.error('Kan niet verbinden met DB ...', err))
 
 console.log('App: ' + config.get('name'))
 console.log('App host: ' + config.get('mail.host'))
 
-app.use(router);
 
 app.listen(port, ()=> console.log('Listening on port ' + port ))
---
+++ routes/parkings.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const router = express.Router();
+const parking = require('../models/parkingModel');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+
+
+
+async function updateParking(id, name, stad, plaatsen){
+    const result = await parking.findByIdAndUpdate(
+        {_id: id},
+        {$set:{
+            name: name,
+            stad: stad,
+            plaatsen: plaatsen
+        }
+    }
+    )
+    console.log(result);
+}
+
+async function deleteParking(id){
+    const result = await parking.findByIdAndDelete(id);
+    console.log(result);
+}
+
+
+
+
+async function getParkings() {
+    try{
+        const parkings = await parking.find({});
+        return {parkings};
+    } catch(err){
+        console.error(err);
+        return {error: err.message};
+    }
+}
+
+
+
+router.get('/parkings', async (req, res) => {
+    try{
+        res.status(200).json((await getParkings()).parkings);
+    } catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+
+
+router.get('/parkings/:id', async (req, res) => {
+    try{
+        const parkings = (await getParkings()).parkings;
+
+
+
+        const parkingById = games.find(parking => parking._id.toString() === req.params.id);
+
+        if(!parkingById) return res.status(404).json({error: 'Parking not found'});
+
+        res.send(parkingById);
+    } catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+
+router.post('/parkings', async (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        stad: Joi.string().required(),
+        plaatsen: Joi.number().required()
+    })
+
+
+    const result = schema.validate(req.body);
+    if(result.error){
+        return res.status(400).json({error: result.error.details[0].message});
+    }
+
+    const findByName = await parking.findOne({name: req.body.name});
+
+    if(findByName){
+        return res.status(400).json({error: 'Parking already exists'});
+    }
+
+    const parkeer = new parking({
+        name: req.body.name,
+        stad: req.body.stad,
+        plaatsen: req.body.plaatsen
+    })
+
+    try{
+        const result = await parkeer.save();
+        res.send(result);
+    } catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.put('/parkings/:id', async (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        stad: Joi.string().required(),
+        plaatsen: Joi.number().required()
+    })
+
+    const parkeerplaats = await parking.findById(req.params.id);
+
+    const result = schema.validate(req.body);
+
+    if(result.error){
+        return res.status(400).json({error: result.error.details[0].message});
+    }
+
+    parkeerplaats.name = req.body.name;
+    parkeerplaats.stad = req.body.stad;
+    parkeerplaats.plaatsen = req.body.plaatsen;
+
+    try{
+        const result = await parkeerplaats.save();
+        res.send(result);
+    } catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+
+
+router.delete('/parkings/:id', async (req, res) => {
+    try{
+        const result = await deleteParking(req.params.id);
+        res.send(result);
+    } catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+
+module.exports = router;
